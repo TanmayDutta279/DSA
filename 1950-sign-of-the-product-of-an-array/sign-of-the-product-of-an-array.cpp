669385659207d8a31d0aa9b860170cfccf8d051f
Tanmay Dutta <146571648+TanmayDutta279@users.noreply.github.com>
2026-09-07 22:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (48.92%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -4,15 +4,11 @@ public:
         int pro = 1;
         for(int i = 0;i<nums.size();i++){
             if(nums[i] == 0){
-                nums[i]=0;
+                return 0;
             }
-            else if(nums[i]>0){
-                nums[i]=1;
+            else if(nums[i]<0){
+                pro*=-1;
             }
-            else{
-                nums[i]=-1;
-            }
-            pro *= nums[i];
         }
         return pro;
     }
