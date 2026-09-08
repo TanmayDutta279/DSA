199ee8fd93a2ae36ff0f1193b8561d177e714612
Tanmay Dutta <146571648+TanmayDutta279@users.noreply.github.com>
2026-09-08 10:17:46 +0530

    Time: 4 ms (32.42%) | Memory: 28.8 MB (35.94%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int n:nums){
+            mp[n]++;
+        }
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                if(mp[nums[i]]==1){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+};
