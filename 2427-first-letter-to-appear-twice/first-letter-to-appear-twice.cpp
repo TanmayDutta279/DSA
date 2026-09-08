ce1495fbf53d0353e7d3eb111a5e0e3fe077d9da
Tanmay Dutta <146571648+TanmayDutta279@users.noreply.github.com>
2026-09-08 10:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (24.61%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -3,15 +3,10 @@ public:
     char repeatedCharacter(string s) {
         unordered_set<char> st;
         for(int i = 0;i<s.size();i++){
-            if(st.empty()){
-                st.insert(s[i]);
-            }
-            else if(st.find(s[i])!=st.end()){
+            if(st.find(s[i])!=st.end()){
                 return s[i];
             }
-            else{
-                st.insert(s[i]);
-            }
+            st.insert(s[i]);
         }
         return s[0];
     }
