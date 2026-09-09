a2dd02b0b7d291ba3ee15ddaf3ca3ecbef8c9643
Tanmay Dutta <146571648+TanmayDutta279@users.noreply.github.com>
2026-09-09 10:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (56.31%) - LeetSync

--- 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int minimumSwap(string s1, string s2) {
-        int x1 = 0,y1 = 0,x2 = 0,y2 = 0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]==s2[i]){
-                continue;
+        int xy = 0;
+        int yx = 0;
+        int n = s1.size();
+        for(int i=0;i<n;i++){
+            if(s1[i] == 'x' && s2[i] == 'y'){
+                xy++;
             }
-            if(s1[i]=='x'){
-                x1++;
-            }
-            else if(s1[i]=='y'){
-                y1++;
-            }
-            if(s2[i]=='x'){
-                x2++;
-            }
-            else if(s2[i]=='y'){
-                y2++;
+            if(s1[i] == 'y' && s2[i] == 'x'){
+                yx++;
             }
         }
-        if((x1+x2)%2!=0 || (y1+y2)%2!=0){
-            return -1;
-        }
-        return x1/2+y1/2+(x1%2)*2;
+        if((xy+yx)%2) return -1;
+        return xy/2 + yx/2 + (xy%2)*2;
     }
 };
